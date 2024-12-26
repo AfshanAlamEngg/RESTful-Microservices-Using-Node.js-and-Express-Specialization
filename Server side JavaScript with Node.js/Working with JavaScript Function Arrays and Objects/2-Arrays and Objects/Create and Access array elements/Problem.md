28470cc007f0371a647ae535c9cea4cf4e7d60b9
AfshanAlamEngg <afshanalamengg@gmail.com>
2024-12-26 22:08:22 +0530

    Added Create and accces array elements files

---
+++ Server side JavaScript with Node.js/Working with JavaScript Function Arrays and Objects/2-Arrays and Objects/Create and Access array elements/Problem.md
@@ -0,0 +1,5 @@
+# Create and Access Array Elements
+
+### Problem Statement
+
+Store and retreive car makes from an array
---
+++ Server side JavaScript with Node.js/Working with JavaScript Function Arrays and Objects/2-Arrays and Objects/Create and Access array elements/js/script.js
@@ -0,0 +1,45 @@
+// // let carMakes = ['Nissan', 'Ford', 'Tata']
+
+// let carMakes = new Array('Nissan', 'Ford', 'Tata');
+
+// console.log(carMakes)
+
+// carMakes.push('BMW')
+
+// console.log('After pushing BMW to the list')
+
+// console.log(carMakes)
+
+// carMakes.unshift('Audi')
+
+// console.log('After adding Audi to the beginning of the list')
+
+// console.log(carMakes)
+
+// carMakes.pop()
+
+// console.log('After popping Car Make')
+
+// console.log(carMakes)
+
+// carMakes.shift()
+
+// console.log('After removing Car Make from the beginning')
+
+// console.log(carMakes)
+
+let cars  = [];
+
+// make, model, color
+let make = 'Eagle';
+let model = 'Talon TSI';
+let color = 'Blue';
+
+let car = [];
+car.push(make);
+car.push(model);
+car.push(color);
+
+cars.push(car)
+
+console.log(cars)
